f359418b32e2bec839315eb8497476306aed029f
shukri1236 <shukri1236>
2024-07-13 11:56:01 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Your-Name is the champion';
+newHeader.textContent = 'Reen is the champion';
+
+document.body.appendChild(newHeader);
+document.body.appendChild(newHeader);
